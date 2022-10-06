b74c08f563e0e1b961e3306d0fdfb5aade46bcfc
annieww <annie9016@hotmail.com>
2022-10-06 15:45:34 +1100

    add event swiper, tag, and pet cards on index page

--- pages/pets/index.js
+++ pages/pets/index.js
@@ -5,6 +5,50 @@ Page({
    * Page initial data
    */
   data: {
+    events: [
+      {
+        name: "Adoption Day",
+        location: "Jing'An",
+        time: "October 30th",
+        imageurl: "https://images.unsplash.com/photo-1514373941175-0a141072bbc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+      },
+      {
+        name: "Dogs of the month",
+        imageurl: "https://images.unsplash.com/photo-1516222338250-863216ce01ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+      },
+      {
+        name: "Cats of the month",
+        imageurl: "https://images.unsplash.com/photo-1594142404563-64cccaf5a10f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+      },
+      {
+        name: "Adoption FAQ",
+        imageurl: "https://images.unsplash.com/photo-1516734212186-a967f81ad0d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+      }
+    ],
+
+    pets: [
+      {
+        name: "Dobby",
+        sex: "boy",
+        imageurl: "https://images.unsplash.com/photo-1433162653888-a571db5ccccf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+      },
+      {
+        name: "Tiger",
+        sex: "girl",
+        imageurl: "https://images.unsplash.com/photo-1553545999-8621dce8d75e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
+      }
+    ],
+
+    tags: [
+      {
+        name: "foster",
+        imageurl: "/images/foster.png"
+      },
+      {
+        name: "adopt",
+        imageurl: "/images/adopt.png"
+      }
+    ]
 
   },
 
@@ -26,7 +70,7 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    const pets = app.globalData.pets
   },
 
   goToFAQ() {
@@ -35,6 +79,14 @@ Page({
     })
   },
 
+  selectTag(e) {
+    if (this.data.tag !== e.currentTarget.dataset.tag) {
+      this.setData({
+        tag: e.currentTarget.dataset.tag
+      })
+      this.getNewData()
+    }
+  },
   /**
    * Lifecycle function--Called when page hide
    */
