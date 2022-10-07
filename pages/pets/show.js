e6020ac60bcefb4cd1bb029d4809401fc29e884c
annieww <annie9016@hotmail.com>
2022-10-07 15:15:03 +1100

    add show page

--- pages/pets/show.js
+++ pages/pets/show.js
@@ -1,18 +1,34 @@
 // pages/pets/show.js
+const app = getApp()
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    pets: []
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-
+    console.log('options from onLoad', options)
+    let id = options.id
+    let page = this
+    // Get api data
+    wx.request({
+      url: `http://localhost:3000/api/v1/pets/${id}`,
+      // method: 'GET',
+      success(res) {
+        console.log({res})
+        const pet = res.data;
+        console.log("Pet:" + res.data)
+        // Update local data
+        page.setData({pet: pet});
+        // wx.hideToast();
+      }
+    })
   },
 
   /**
@@ -25,8 +41,33 @@ Page({
   /**
    * Lifecycle function--Called when page show
    */
-  onShow() {
+  getData() {
+    let page = this
+    console.log('from show.js = onshow: options', page.options)
+    let id = page.options.id
+    wx.request({
+      header: app.globalData.header,
+      url: `${app.globalData.baseURL}/pets/${id}`,
+      success(res) {
+        console.log("From show.js - onshow: res",res)
+        if (res.statusCode === 200) {
+          const pet = res.data.pet;
+          page.setData({
+            pet: pet
 
+          });
+          console.log("From show.js: status code is", res.statusCode)
+        }
+      }
+    })
+  },
+
+  onShow() {
+    if (app.globalData.header) {
+      this.getData()
+    } else {
+      wx.event.on('loginFinished', this, this.getData)
+    }
   },
 
   /**
