a1fbea1848d24184cde8acd519f559f51af9e788
annieww <annie9016@hotmail.com>
2023-01-18 18:51:33 +1100

    add CN text to frontend

--- pages/user/profile.js
+++ pages/user/profile.js
@@ -39,7 +39,9 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    this.setData({
+      content: app.globalData.content
+    })
   },
 
   goToForm() {
--- utils/en.js
+++ utils/en.js
@@ -5,7 +5,13 @@ let en = {
   text4: 'What to do when you found a stray/lost pet in the street?',
   text5: 'What should you know about adopting pets? ',
   text6: 'Who to contact if you want help out the organization? ',
-  text7: 'Frequently Asked Questions'
+  text7: 'Frequently Asked Questions',
+  text8: 'tell us a bit more about yourself by completing your general information.',
+  text9: 'complete my profile',
+  text10: 'my info',
+  text11: 'my status',
+  text12: 'my adoption requests',
+  text13: 'my favorites'
 }
 module.exports = {
   content: en
